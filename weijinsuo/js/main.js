45fb59a7a055388f2244da6240af21a1f93b2106
ZHANG Kai <kai.zhang@soprasteria.com>
2018-12-05 20:22:22 +0800

    Products section

--- weijinsuo/js/main.js
+++ weijinsuo/js/main.js
@@ -8,22 +8,58 @@
 'use strict';
 
 $(function() {
-	let imgResize = () => {
-		const windowWidth = window.innerWidth;
-		const isSmallScreen = windowWidth < 768;
+	let screenResize = () => {
+		let imgResize = () => {
+			const windowWidth = window.innerWidth;
+			const isSmallScreen = windowWidth < 768;
 
-		$('#carousel-zk > .carousel-inner > .item').each(function(index) {
-			const imgSrc = $(this).data(isSmallScreen ? 'imageXs' : 'imageLg');
-			// console.log(imgSrc);
-			const bgImg = `url("${imgSrc}")`;
-			// console.log(bgImg);
-			if (isSmallScreen) {
-				$(this).html(`<img src="${imgSrc}" alt="pic" />`);
+			$('#carousel-zk > .carousel-inner > .item').each(function(index) {
+				const imgSrc = $(this).data(isSmallScreen ? 'imageXs' : 'imageLg');
+				// console.log(imgSrc);
+				const bgImg = `url("${imgSrc}")`;
+				// console.log(bgImg);
+				if (isSmallScreen) {
+					$(this).html(`<img src="${imgSrc}" alt="pic" />`);
+				} else {
+					$(this).css('backgroundImage', bgImg);
+					$(this).html('');
+				}
+			});
+		}
+
+		imgResize();
+
+		/*
+		 * control products touch scroll ul
+		 */
+		let ulWidthControl = () => {
+			const $navTabs = $('.nav-tabs');
+			let tabWidth = 40;
+			$('.nav-tabs>li').each(function() {
+				tabWidth += $(this).width();
+				// console.log(tabWidth);
+			});
+			// console.log('tabwidth= ' + tabWidth);
+			const ulScrollWidth = $('.ul-scroll').width();
+			// console.log('ulScrollWidth= ' + ulScrollWidth)
+			if (tabWidth > ulScrollWidth) {
+				$navTabs.css('width', tabWidth)
+					.parent()
+					.css('overflow-x', 'scroll');;
 			} else {
-				$(this).css('backgroundImage', bgImg);
-				$(this).html('');
+				$navTabs.css('width', 'auto')
+					.parent()
+					.css('overflow-x', 'hidden');;
+
 			}
-		});
+		}
+
+		ulWidthControl();
 	}
-	$(window).on('resize', imgResize).trigger('resize');
+
+	$(window).on('resize', screenResize).trigger('resize');
+
+
+
+
 });
