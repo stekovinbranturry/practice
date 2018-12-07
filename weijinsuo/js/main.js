1df43a47a111a0572ba2cffe9f5a467bac406a13
ZHANG Kai <kai.zhang@soprasteria.com>
2018-12-07 11:56:53 +0800

    Touch swipe carousel

--- weijinsuo/js/main.js
+++ weijinsuo/js/main.js
@@ -8,67 +8,95 @@
 'use strict';
 
 $(function() {
-    let screenResize = () => {
-        let imgResize = () => {
-            const windowWidth = window.innerWidth;
-            const isSmallScreen = windowWidth < 768;
+	let screenResize = () => {
+		let imgResize = () => {
+			const windowWidth = window.innerWidth;
+			const isSmallScreen = windowWidth < 768;
 
-            $('#carousel-zk > .carousel-inner > .item').each(function(index) {
-                const imgSrc = $(this).data(isSmallScreen ? 'imageXs' : 'imageLg');
-                // console.log(imgSrc);
-                const bgImg = `url("${imgSrc}")`;
-                // console.log(bgImg);
-                if (isSmallScreen) {
-                    $(this).html(`<img src="${imgSrc}" alt="pic" />`);
-                } else {
-                    $(this).css('backgroundImage', bgImg);
-                    $(this).html('');
-                }
-            });
-        };
+			$('#carousel-zk > .carousel-inner > .item').each(function(index) {
+				const imgSrc = $(this).data(isSmallScreen ? 'imageXs' : 'imageLg');
+				// console.log(imgSrc);
+				const bgImg = `url("${imgSrc}")`;
+				// console.log(bgImg);
+				if (isSmallScreen) {
+					$(this).html(`<img src="${imgSrc}" alt="pic" />`);
+				} else {
+					$(this).css('backgroundImage', bgImg);
+					$(this).html('');
+				}
+			});
+		};
 
-        imgResize();
+		imgResize();
 
-        /*
-         * control products touch scroll ul
-         */
-        let ulWidthControl = () => {
-            const $navTabs = $('.nav-tabs');
-            let tabWidth = 40;
-            $('.nav-tabs>li').each(function() {
-                tabWidth += $(this).width();
-                // console.log(tabWidth);
-            });
-            // console.log('tabwidth= ' + tabWidth);
-            const ulScrollWidth = $('.ul-scroll').width();
-            // console.log('ulScrollWidth= ' + ulScrollWidth)
-            if (tabWidth > ulScrollWidth) {
-                $navTabs.css('width', tabWidth)
-                    .parent()
-                    .css('overflow-x', 'scroll');
-            } else {
-                $navTabs.css('width', 'auto')
-                    .parent()
-                    .css('overflow-x', 'hidden');
+		/*
+		 * control products touch scroll ul
+		 */
+		let ulWidthControl = () => {
+			const $navTabs = $('.nav-tabs');
+			let tabWidth = 40;
+			$('.nav-tabs>li').each(function() {
+				tabWidth += $(this).width();
+				// console.log(tabWidth);
+			});
+			// console.log('tabwidth= ' + tabWidth);
+			const ulScrollWidth = $('.ul-scroll').width();
+			// console.log('ulScrollWidth= ' + ulScrollWidth)
+			if (tabWidth > ulScrollWidth) {
+				$navTabs.css('width', tabWidth)
+					.parent()
+					.css('overflow-x', 'scroll');
+			} else {
+				$navTabs.css('width', 'auto')
+					.parent()
+					.css('overflow-x', 'hidden');
 
-            }
-        };
+			}
+		};
 
-        ulWidthControl();
-    };
+		ulWidthControl();
+	};
 
-    $(window).on('resize', screenResize).trigger('resize');
+	$(window).on('resize', screenResize).trigger('resize');
 
-    /*
-     * change news title
-     */
-    let changeNewsTitle = () => {
-        $('.nav-stacked').on('click', 'a', function() {
-            const $newsTitle = $(this).data('title');
-            $('.news-title').html($newsTitle);
+	/*
+	 * change news title
+	 */
+	let changeNewsTitle = () => {
+		$('.nav-stacked').on('click', 'a', function() {
+			const $newsTitle = $(this).data('title');
+			$('.news-title').html($newsTitle);
 
-        });
-    };
+		});
+	};
 
-    changeNewsTitle();
+	changeNewsTitle();
+
+	/*
+	 * touch swipe carousel
+	 */
+
+	let carouselSwipe = (start, end, offset) => {
+		const isLeft = (start - end - offset) > 0;
+		const isRight = (end - start - offset) > 0;
+		if (isLeft) {
+			$('.carousel').carousel('prev');
+		}
+
+		if (isRight) {
+			$('.carousel').carousel('next');
+		}
+	};
+
+	$('.carousel').on('touchstart', function(e) {
+		let startX, endX = 0;
+		startX = e.touches[0].clientX;
+		$(this).on('touchmove', function(e) {
+			endX = e.touches[0].clientX;
+		}).on('touchend', function(e) {
+			$(this).off('touchmove touchend'); // this is very important!!!
+			carouselSwipe(startX, endX, 30);
+		});
+	});
+
 });
