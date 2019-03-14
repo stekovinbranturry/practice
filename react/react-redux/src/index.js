7c54c1c4984bab92ecf386c8a920faa7a645e531
Zhang Kai <zhangkai930418@gmail.com>
2019-03-14 16:34:01 +0800

    update

--- react/react-redux/src/index.js
+++ react/react-redux/src/index.js
@@ -1,23 +1,10 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
-import { Provider } from './react-redux'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import Header from './Header'
 import Content from './Content'
 import './index.css'
-
-function createStore(reducer) {
-  let state = null
-  const listeners = []
-  const subscribe = (listener) => listeners.push(listener)
-  const getState = () => state
-  const dispatch = (action) => {
-    state = reducer(state, action)
-    listeners.forEach((listener) => listener())
-  }
-  dispatch({}) // 初始化 state
-  return { getState, dispatch, subscribe }
-}
 
 const themeReducer = (state, action) => {
   if (!state) return {
