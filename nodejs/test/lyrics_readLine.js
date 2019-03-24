b18b1c5df331bca35ecfa1b7996e242ff119ab9b
Zhang Kai <zhangkai930418@gmail.com>
2019-03-25 00:57:36 +0800

    read lyrics

--- nodejs/test/lyrics_readLine.js
+++ nodejs/test/lyrics_readLine.js
@@ -1,15 +1,18 @@
 /*
  * @Author: Zhang Kai 
- * @Date: 2019-03-22 14:46:33 
+ * @Date: 2019-03-23 22:12:41 
  * @Last Modified by: Zhang Kai
- * @Last Modified time: 2019-03-23 22:25:52
+ * @Last Modified time: 2019-03-23 22:26:24
  */
 
 const fs = require('fs');
 const path = require('path');
-// const iconv = require('iconv-lite');
+const readline = require('readline');
 
 const lyricPath = path.join(__dirname, './lyrics/王菲 - 爱不可及.lrc');
+const readStream = fs.createReadStream(lyricPath);
+const rl = readline.createInterface({ input: readStream });
+
 const regex = /\[(\d{2})\:(\d{2})\.(\d{2})\](.+)/;
 const startTime = new Date().getTime();
 
@@ -27,10 +30,6 @@ const readLyrics = (line, regex, startTime) => {
     }
 };
 
-fs.readFile(lyricPath, (err, data) => {
-    const lines = data.toString('utf8').split('\n');
-    lines.forEach((line) => {
-        readLyrics(line, regex, startTime);
-    });
-});
-
+rl.on('line', (line) => {
+    readLyrics(line, regex, startTime);
+})
