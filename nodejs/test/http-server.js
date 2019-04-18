dcc61b581e34774dbcde98893140648f3f52f63b
Zhang Kai <zhangkai930418@gmail.com>
2019-04-18 21:54:49 +0800

    http server

--- nodejs/test/http-server.js
+++ nodejs/test/http-server.js
@@ -2,7 +2,7 @@
  * @Author: Zhang Kai 
  * @Date: 2019-03-26 11:08:36 
  * @Last Modified by: Zhang Kai
- * @Last Modified time: 2019-03-26 12:03:48
+ * @Last Modified time: 2019-04-18 21:53:55
  */
 
 const http = require('http');
@@ -10,8 +10,10 @@ const http = require('http');
 const [hostname, port] = ['127.0.0.1', 8080];
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World');
+    console.log(request.url);
+    response.writeHead(200, { 'content-type': 'text/html' });
+    response.write(`<h1>Hello World</h1>`)
+    response.end();
 });
 
 server.listen(port, hostname, () => {
